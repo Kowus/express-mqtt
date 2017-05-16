d18ae810911d9e95478b7cd8d444a31dde63a08a
kowus <barnabas@elite-education.org>
2017-05-16 13:19:43 +0000

    render temperature charts

--- public/javascripts/app/app.js
+++ public/javascripts/app/app.js
@@ -12,23 +12,39 @@
 				"xaxisname": "Date/Time",
 				"yaxisname": "Amount in millimeters",
 				"numdisplaysets": "10",
-				"labeldisplay": "rotate",
+				"labeldisplay": "rotate"
 			},
 			"data":[]
 		};
+		$scope.heat = temperature;
 		var handleCallback = function (msg) {
 			$scope.$apply(function () {
 				var tempJS=JSON.parse(msg.data);
 				var dt = new Date(tempJS.date);
 				var date_time= dt.getUTCDate() + '-'+(dt.getUTCMonth()+1).toString()+'-'+dt.getUTCFullYear()+'@'+dt.getUTCHours()+':'+dt.getUTCMinutes()+':'+dt.getSeconds()
 				$scope.rain.data.push({value:tempJS.rain,label:date_time});
-				console.log($scope.rain);
+				$scope.heat.data.push({value:tempJS.temperature,label:date_time});
+				// console.log($scope.rain);
 			});
 		};
 
 		var source = new EventSource('/stream');
 		source.addEventListener('message', handleCallback, false)
-
 	}]);
+
+	var temperature = {
+		"chart": {
+			"caption": "Temperature",
+			"subcaption": "Current Temperature",
+			"xaxisname": "Date/Time",
+			"yaxisname": "Temperature in Degrees Celcius (°C)",
+			"subcaptionFontBold": "0",
+			"numberSuffix": "°C",
+			"theme": "fint",
+			"labeldisplay": "rotate",
+			"divlinecolor":"#cccccc"
+		},
+		"data":[]
+	}
 
 })();
